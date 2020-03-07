02b002f5d0296888bd7870865dc1fecc37b21864
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2020-03-07 02:00:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
+
 # Write your code here.
+
+def line(katz_deli)
+  line = []
+  
+    if katz_deli.length == 0 
+      puts "The line is currently empty."
+    else
+      
+      katz_deli.each_with_index do |name, index|
+        line << "#{index +1}. #{name}"
+      end
+      
+      puts "The line is currently: " + line.join(" ")
+    end
+
+end
+
+
+
+def take_a_number(katz_deli,new_person)
+  
+end
